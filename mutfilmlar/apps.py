738473b528697719c0a0e741e644c374cb944fb2
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-02-28 02:03:13 -0800

    create uzmovi movies

---
+++ mutfilmlar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MutfilmlarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mutfilmlar'
---
+++ qoidalar/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QoidalarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'qoidalar'
