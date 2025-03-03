3835ab8c66fa9d085e31abe610ab3f660b8f0342
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-03 01:34:43 -0800

    nav

--- uzmovi/views.py
+++ uzmovi/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-#Post.Get.Delete.Update.Create
+def home(request):
+    return render(request, 'base.html')
 
-#Edit -- Tahrirlash
+def NavBar(request):
+     return render(request, 'navbar.html')
+
+
 
