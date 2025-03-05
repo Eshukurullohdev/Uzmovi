4bed75d365377912941fafdff843b9038455f0fc
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-05 13:33:13 +0500

    others update

--- uzmovi/views.py
+++ uzmovi/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 def NavBar(request):
      return render(request, 'navbar.html')
-
+ 
 
 
