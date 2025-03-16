16ea27e2bd91afd96d2c5f335d6ed403a69a68a5
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-16 17:12:50 +0500

    opp

--- uzmovi/views.py
+++ uzmovi/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.contrib import messages
 def home(request):
-    if not request.session.get('message_shown'):  # Xabar ko'rsatilganligini tekshiramiz
-        messages.success(request, "Siz saytga kirdingiz!")  # Xabar chiqarish
-        request.session['message_shown'] = True
+      # Xabar ko'rsatilganligini tekshiramiz
+    messages.success(request, "Siz saytga kirdingiz!")  # Xabar chiqarish
+        
     return render(request, 'home.html')
 
 def NavBar(request):
