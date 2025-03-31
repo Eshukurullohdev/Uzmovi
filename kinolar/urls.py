08cdd2d5a25b176d715aa09a298bf9d92ced06bc
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-03-31 16:59:22 +0500

    main movie

--- kinolar/urls.py
+++ kinolar/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . views import DetailMovie
 urlpatterns = [
-    path('detail/', DetailMovie, name='detail'),
+    path('movie/<int:movie_id>/', DetailMovie, name='detail_movie'),
 ]
--- kinolar/views.py
+++ kinolar/views.py
@@ -1,8 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from .models import Movie
-def DetailMovie(request):
-    try:
-        kino = Movie.objects.all()
-    except Movie.DoesNotExist:
-        kino = None
-    return render(request, 'detailmovie.html', {'kino': kino})  
+def DetailMovie(request, movie_id):
+    kino = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'detailmovie.html', {'kino': kino})
