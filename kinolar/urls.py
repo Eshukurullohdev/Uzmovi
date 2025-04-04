da392cde6f4952c19a1aa8e3cf96ec31618178e6
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-04 14:41:29 +0500

    swiper js

--- kinolar/urls.py
+++ kinolar/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . views import DetailMovie
 urlpatterns = [
-    path('movie/<int:movie_id>/', DetailMovie, name='detail_movie'),
+    path('movie/<int:movie_id>/', DetailMovie, name='detail_movie')
 ]
--- kinolar/views.py
+++ kinolar/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render,  get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
 def DetailMovie(request, movie_id):
     kino = get_object_or_404(Movie, id=movie_id)
     return render(request, 'detailmovie.html', {'kino': kino})
+
