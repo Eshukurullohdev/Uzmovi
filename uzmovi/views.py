9d27aa1b53da994c0883480bff96c6d3d790ab07
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-07 14:44:58 +0500

    filter carton

--- uzmovi/views.py
+++ uzmovi/views.py
@@ -3,9 +3,9 @@ from django.contrib import messages
 from kinolar.models import Movie
 def home(request):
     kino = Movie.objects.all()
-    multfilmalr = Movie.objects.filter(categorykinolar__categorykino__iexact='Multfilmlar')
+    multfilmlar = Movie.objects.filter(categorykinolar__categorykino__iexact='Multfilmlar')
     messages.success(request, "Siz bosh sahifaga keldingiz")
-    return render(request, 'home.html', {'kino': kino, 'multfilmalr': multfilmalr})
+    return render(request, 'home.html', {'kino': kino, 'multfilmlar': multfilmlar})
 
 def NavBar(request):
      return render(request, 'navbar.html')
