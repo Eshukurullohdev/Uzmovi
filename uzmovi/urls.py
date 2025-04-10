fba7b696d25326af6fc6811d11a13e4b06944504
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-10 22:09:48 +0500

    year of movies

--- uzmovi/urls.py
+++ uzmovi/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+     path('movies/year/<str:yil>/', movieyear, name='movie_by_year'),
 ]
--- uzmovi/views.py
+++ uzmovi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from kinolar.models import Movie
+from kinolar.models import Movie, CategoryYili
 def home(request):
     kino = Movie.objects.all()
     multfilmlar = Movie.objects.filter(categorykinolar__categorykino__iexact='Multfilmlar')
@@ -8,7 +8,14 @@ def home(request):
     return render(request, 'home.html', {'kino': kino, 'multfilmlar': multfilmlar})
 
 def NavBar(request):
-     return render(request, 'navbar.html')
+    return render(request, 'navbar.html')
+
+def movieyear(request, yil):
+    movieyil = Movie.objects.filter(categoryyili__categoryyili__iexact=yil)
+    return render(request, 'movie_by_year.html', {
+        'movieyil': movieyil,
+        'yil': yil
+    })
  
 
 
